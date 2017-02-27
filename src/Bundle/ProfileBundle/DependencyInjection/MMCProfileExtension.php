9f1b75d07169d4d54d20f561509ea25f6ce038b3
ldousselain <l.dousselain@meuhmeuhconcept.com>
2017-02-27 10:00:12 +0100

    Use prepend function to give stofDoctrineExtensions conf

--- src/Bundle/ProfileBundle/DependencyInjection/MMCProfileExtension.php
+++ src/Bundle/ProfileBundle/DependencyInjection/MMCProfileExtension.php
@@ -4,10 +4,11 @@ namespace MMC\Profile\Bundle\ProfileBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class MMCProfileExtension extends Extension
+class MMCProfileExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -23,5 +24,36 @@ class MMCProfileExtension extends Extension
         $container->setParameter('mmc_profile.profile_class', $config['profile_class']);
         $container->setParameter('mmc_profile.userProfile_class', $config['userProfile_class']);
         $container->setParameter('mmc_profile.user_class', $config['user_class']);
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $doctrine = [
+            'orm' => [
+                'entity_managers' => [
+                    'default' => [
+                        'filters' => [
+                            'softdeleteable' => [
+                                'class' => 'Gedmo\SoftDeleteable\Filter\SoftDeleteableFilter',
+                                'enabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $container->prependExtensionConfig('doctrine', $doctrine);
+
+        $stof_doctrine_extensions = [
+            'default_locale' => '%locale%',
+            'orm' => [
+                'default' => [
+                    'softdeleteable' => true,
+                ],
+            ],
+        ];
+
+        $container->prependExtensionConfig('stof_doctrine_extensions', $stof_doctrine_extensions);
     }
 }
