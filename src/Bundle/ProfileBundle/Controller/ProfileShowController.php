80d7ee6e80120f1c82dd1dc032afa970130dcbdf
ldousselain <l.dousselain@meuhmeuhconcept.com>
2017-03-14 12:33:35 +0100

    Change show route - update related files

--- src/Bundle/ProfileBundle/Controller/ProfileShowController.php
+++ src/Bundle/ProfileBundle/Controller/ProfileShowController.php
@@ -3,31 +3,34 @@
 namespace MMC\Profile\Bundle\ProfileBundle\Controller;
 
 use MMC\Profile\Component\Model\ProfileInterface;
-use MMC\Profile\Component\Model\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Templating\EngineInterface;
 
 /**
- * @Route("/profile/show", service="profile_bundle.profile_show_controller")
+ * @Route("/profile", service="profile_bundle.profile_show_controller")
  */
 class ProfileShowController
 {
     private $templating;
+    private $tokenStorage;
 
     public function __construct(
-        EngineInterface $templating
+        EngineInterface $templating,
+        TokenStorage $tokenStorage
     ) {
         $this->templating = $templating;
+        $this->tokenStorage = $tokenStorage;
     }
 
     /**
      * @ParamConverter("profile", class="AppBundle:Profile")
-     * @ParamConverter("user", class="AppBundle:User")
-     * @Route("/{uuid}/{username}", name="profile_bundle_show_profile")
+     * @Route("/{uuid}", name="profile_bundle_show_profile")
      */
-    public function show(ProfileInterface $profile, UserInterface $user)
+    public function show(ProfileInterface $profile)
     {
+        $user = $this->tokenStorage->getToken()->getUser();
         $up = $profile->getUserProfile($user);
 
         return $this->templating->renderResponse('AppBundle:Profile:profile.html.twig',
